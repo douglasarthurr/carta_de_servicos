1c417d198f09455dafbe9f2f4d814b1d0660b448
Douglas Bento <douglasarthur.s.bento@gmail.com>
2024-04-02 15:46:54 -0400

    configurações gerais

---
+++ servicos-publicos/src/layout/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg'; exemplo de como importar imagem
+
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
