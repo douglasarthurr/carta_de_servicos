d05d6df831ff6d6a945f31e1966a13cfa9a3bf1d
Douglas Arthur <douglasarthur.s.bento@gmail.com>
2024-04-05 16:36:21 -0400

    ajustes bugs

--- servicos-publicos/src/layout/Header.jsx
+++ servicos-publicos/src/layout/Header.jsx
@@ -8,7 +8,7 @@ function Header () {
                 <img src={logo} alt="logo" id="logo"/>
                 <div class="texts">
                     <h1 id="title">Serviços Públicos</h1>
-                    <a href="https://pt.wikipedia.org/wiki/Florianópolis" target="_blank" id="subtitle">Florianópolis</a>
+                    <a href="https://pt.wikipedia.org/wiki/Florianópolis" target="_blank"  rel="noopener noreferrer" id="subtitle">Florianópolis</a>
                 </div>
             </header>
             <div>
