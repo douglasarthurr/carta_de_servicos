3c72f14d6a5696f7559360746383019e913fe07e
Douglas Arthur <douglasarthur.s.bento@gmail.com>
2024-04-04 22:16:04 -0400

    header finalizado

--- servicos-publicos/src/layout/Header.jsx
+++ servicos-publicos/src/layout/Header.jsx
@@ -1,8 +1,14 @@
+import logo from "../images/logo.png"
+import "../css/Header.css"
+
 function Header () {
     return(
-        <header>
-            <h1>Serviços Públicos</h1>
-            <p>Florianópolis</p>
+        <header class="header">
+            <img src={logo} alt="logo" id="logo"/>
+            <div class="texts">
+                <h1 id="title">Serviços Públicos</h1>
+                <a href="https://pt.wikipedia.org/wiki/Florianópolis" target="_blank" id="subtitle">Florianópolis</a>
+            </div>
         </header>
     );
 }
