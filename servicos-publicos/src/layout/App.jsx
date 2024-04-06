4dd842df6780845f87105a5029a4bed832fb881d
Douglas Arthur <douglasarthur.s.bento@gmail.com>
2024-04-06 11:10:15 -0400

    finalização do projeto

--- servicos-publicos/src/layout/App.jsx
+++ servicos-publicos/src/layout/App.jsx
@@ -19,25 +19,28 @@ function App() {
       <header className="App-header">
         <Header></Header>
       </header>
-      <Servicos image={jornada} title='Ampliação de jornada' description= 'Estão qualificar a aprendizagem de crianças e adolescentes e melhorar os indicadores educacionais.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=4620'/>
-      
-      <Servicos image={imobiliario} title='Certidão de cadastro imobiliário para fins gerais' description= 'Referente ao documento que certifica a situação cadastral atual do imóvel cadastrado sob uma inscrição imobiliária específica.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&id=4260&menu=2'/>
-      
-      <Servicos image={financeiro} title='Auxílio Financeiro' description= ' É o benefício estabelecido pela Lei 4762/95 concedendo o valor de 50% do salário mínimo aos servidores que tenham filhos portadores de deficiência com incapacidade funcional permanente.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&id=233'/>
-      
-      <Servicos image={carga_horaria} title='Redução de carga horária' description= 'A servidora pública municipal efetiva, que comprovadamente seja mãe, tutora, curadora ou responsável pela criação educação e proteção de pessoa portadora de deficiência.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&id=235'/>
-      
-      <Servicos image={acidente_servico} title='Comunicação de acidente em serviço' description= 'Denomina-se acidente em serviço o dano físico ou mental sofrido pelo servidor quese relacione, mediata ou imediatamente, com o exercício das funções.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=5141 '/>
-      
-      <Servicos image={vale_transporte} title='Vale transporte' description= 'É um benefício garantido pela legislação trabalhista. Esse benefício visa custear o deslocamento do profissional de sua casa até o local de trabalho e vice-versa.' link='https://www.setuf.com.br/'/>
-      
-      <Servicos image={medicas} title='Ressarcimento de despesas médicas decorrentes de acidente de trabalho' description= 'O reconhecimento e caracterização da ocorrência de trabalho relativo aos servidores efetivos é competência da Gerência de Perícia Médica.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=244'/>
-      
-      <Servicos image={casa_apoio} title='Casa de passagem para pessoas em situação de rua' description= 'Serviço de acolhimento provisório previsto para pessoas em situação de rua e desabrigo por abandono, migração e ausência de residência ou pessoas em trânsito e sem condições de auto sustento.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=4691'/>
-      
-      <Servicos image={aposentadoria} title='Aposentadoria por invalidez' description= ' O servidor poderá ser aposentado por invalidez, com proventos integrais ou proporcionais, quando comprovada sua incapacidade total e definitiva para o trabalho, não se vislumbrando a possibilidade de uma readaptação funcional.' link='https://www.pmf.sc.gov.br/entidades/sadm/index.php?pagina=servpagina&me
-nu=2&id=246
-'/>
+      <section class="section_servicos">
+        <div class="container_servicos">
+          <Servicos image={jornada} title='Ampliação de jornada' description= 'Estão qualificar a aprendizagem de crianças e adolescentes e melhorar os indicadores educacionais.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=4620'/>
+          
+          <Servicos image={imobiliario} title='Certidão de cadastro imobiliário para fins gerais' description= 'Referente ao documento que certifica a situação cadastral atual do imóvel cadastrado sob uma inscrição imobiliária específica.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&id=4260&menu=2'/>
+          
+          <Servicos image={financeiro} title='Auxílio Financeiro' description= ' É o benefício estabelecido pela Lei 4762/95 concedendo o valor de 50% do salário mínimo aos servidores que tenham filhos portadores de deficiência com incapacidade funcional permanente.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&id=233'/>
+          
+          <Servicos image={carga_horaria} title='Redução de carga horária' description= 'A servidora pública municipal efetiva, que comprovadamente seja mãe, tutora, curadora ou responsável pela criação educação e proteção de pessoa portadora de deficiência.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&id=235'/>
+          
+          <Servicos image={acidente_servico} title='Comunicação de acidente em serviço' description= 'Denomina-se acidente em serviço o dano físico ou mental sofrido pelo servidor quese relacione, mediata ou imediatamente, com o exercício das funções.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=5141 '/>
+          
+          <Servicos image={vale_transporte} title='Vale transporte' description= 'É um benefício garantido pela legislação trabalhista. Esse benefício visa custear o deslocamento do profissional de sua casa até o local de trabalho e vice-versa.' link='https://www.setuf.com.br/'/>
+          
+          <Servicos image={medicas} title='Ressarcimento de despesas médicas decorrentes de acidente de trabalho' description= 'O reconhecimento e caracterização da ocorrência de trabalho relativo aos servidores efetivos é competência da Gerência de Perícia Médica.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=244'/>
+          
+          <Servicos image={casa_apoio} title='Casa de passagem para pessoas em situação de rua' description= 'Serviço de acolhimento provisório previsto para pessoas em situação de rua e desabrigo por abandono, migração e ausência de residência ou pessoas em trânsito e sem condições de auto sustento.' link='https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=4691'/>
+          
+          <Servicos image={aposentadoria} title='Aposentadoria por invalidez' description= ' O servidor poderá ser aposentado por invalidez, com proventos integrais ou proporcionais, quando comprovada sua incapacidade total e definitiva para o trabalho, não se vislumbrando a possibilidade de uma readaptação funcional.' link='https://www.pmf.sc.gov.br/entidades/sadm/index.php?pagina=servpagina&menu=2&id=246'/>
+        </div>
+    </section>
+    <p id='developed '>developed by Douglas and Jefferson</p>
       
       
     </div>
--- servicos-publicos/src/layout/Header.jsx
+++ servicos-publicos/src/layout/Header.jsx
@@ -5,7 +5,10 @@ function Header () {
     return( 
         <div>
             <header class="header">
-                <img src={logo} alt="logo" id="logo"/>
+                <a href="https://www.pmf.sc.gov.br/servicos/index.php?pagina=servpagina&acao=open&id=4620" target="_blank" rel="noopener noreferrer">
+                    <img src={logo} alt="logo" id="logo"/>
+                </a>
+                
                 <div class="texts">
                     <h1 id="title">Serviços Públicos</h1>
                     <a href="https://pt.wikipedia.org/wiki/Florianópolis" target="_blank"  rel="noopener noreferrer" id="subtitle">Florianópolis</a>
@@ -13,8 +16,8 @@ function Header () {
             </header>
             <div>
                 <p id="description">Aqui você encontra algumas informações dos serviços da cidade de Florianópolis-SC</p>
-                <p id="developed">Developed by Douglas and Jefferson</p>
             </div>
+         
         </div>
     );
 }
