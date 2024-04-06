e67d7fb851ae267797da3422fcf963fcd7ded0ef
Douglas Arthur <douglasarthur.s.bento@gmail.com>
2024-04-06 11:58:57 -0400

    ajustes deploy

--- servicos-publicos/src/layout/Header.jsx
+++ servicos-publicos/src/layout/Header.jsx
@@ -9,6 +9,7 @@ function Header () {
                     <img src={logo} alt="logo" id="logo"/>
                 </a>
                 
+                
                 <div class="texts">
                     <h1 id="title">Serviços Públicos</h1>
                     <a href="https://pt.wikipedia.org/wiki/Florianópolis" target="_blank"  rel="noopener noreferrer" id="subtitle">Florianópolis</a>
